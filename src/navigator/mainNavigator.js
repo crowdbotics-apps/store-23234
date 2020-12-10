d409af58f160b0896a8d416a30dfe55f1b553d8c
Crowdbotics <team@crowdbotics.com>
2020-12-10 14:05:10 +0000

    remove package blueprint-react-native-emailauth-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,7 +5,6 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
-import EmailAuth2183596Navigator from '../features/EmailAuth2183596/navigator';
 import BlankScreen0183009Navigator from '../features/BlankScreen0183009/navigator';
 
 /**
@@ -15,7 +14,6 @@ import BlankScreen0183009Navigator from '../features/BlankScreen0183009/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
-EmailAuth2183596: { screen: EmailAuth2183596Navigator },
 BlankScreen0183009: { screen: BlankScreen0183009Navigator },
 
     /** new navigators can be added here */
