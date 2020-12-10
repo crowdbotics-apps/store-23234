0090e2f0cca867a54c587cd4c064b17ec3301aad
Crowdbotics <team@crowdbotics.com>
2020-12-10 14:17:25 +0000

    Update components

--- src/features/BlankScreen0183009/screens/blank.js
+++ src/features/BlankScreen0183009/screens/blank.js
@@ -24,7 +24,7 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { TextInput_7: "" }
 
   render = () => (
     <View>
@@ -35,8 +35,45 @@ export default class Blank extends React.Component {
         }}
         style={styles.Image_3}
       />
+      <Text>Ingrese el Codigo</Text>
+      <TextInput
+        placeholder="Codigo"
+        style={styles.TextInput_7}
+        value={this.state.TextInput_7}
+        onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
+      />
+      <View style={styles.View_9}>
+        <View style={styles.View_157}>
+          <View style={styles.View_158}>
+            <Image
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/flooop.png"
+              }}
+            />
+          </View>
+          <View style={styles.View_159} />
+        </View>
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, Image_3: { height: 100 } })
+const styles = StyleSheet.create({
+  View_1: {},
+  Image_3: { height: 100 },
+  Text_5: {},
+  TextInput_7: {
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    borderRadius: 4
+  },
+  View_9: { width: 304 },
+  View_157: { flexDirection: "row" },
+  View_158: { width: 150 },
+  Image_162: {},
+  View_159: { width: 150 }
+})
