9ceb108ca0dfef2564a5b38739aef55b4534bb11
Crowdbotics <team@crowdbotics.com>
2020-12-16 16:06:34 +0000

    Update components

--- src/features/BlankScreen2186091/screens/blank.js
+++ src/features/BlankScreen2186091/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,71 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_14: "", TextInput_10: "" }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Image
+        source={{
+          uri:
+            "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/flooop_rbz1A7z.png"
+        }}
+      />
+      <View style={styles.View_5}>
+        <Text>Usuario</Text>
+        <TextInput
+          placeholder="Usuario"
+          value={this.state.TextInput_14}
+          onChangeText={nextValue => this.setState({ TextInput_14: nextValue })}
+        />
+        <Text>Contraseña</Text>
+        <TextInput
+          placeholder="Password"
+          value={this.state.TextInput_10}
+          onChangeText={nextValue => this.setState({ TextInput_10: nextValue })}
+        />
+        <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  Image_3: {},
+  View_5: {
+    width: "100%",
+    height: "100%",
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 60,
+    paddingBottom: 0,
+    alignSelf: "center",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    alignContent: "stretch"
   },
-});
+  Text_8: {},
+  TextInput_14: {},
+  Text_12: {},
+  TextInput_10: {},
+  Button_16: {}
+})
